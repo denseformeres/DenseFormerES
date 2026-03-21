35437f9d18dfc682a924f2276d6a9dea87bfc7ac
denseformeres <denseformeres@yahoo.com>
2026-03-21 10:35:17 +0000

    1

--- experiments/models/denseformeres.py
+++ experiments/models/denseformeres.py
@@ -299,7 +299,7 @@ class DenseformerES(nn.Module):
             idx, pos_emb_closure = self.transformer.wpe(idx)
 
         print("idx min/max:", idx.min().item(), idx.max().item())
-        print("vocab size:", model.config.vocab_size)
+        print("vocab size:", config.vocab_size)
         assert idx.max().item() < model.config.vocab_size, "token index out of bounds!"
         assert idx.min().item() >= 0, "token index negative!"
         tok_emb = self.transformer.wte(idx)
