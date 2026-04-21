a5442effb65247b652a91f1ba76a9d20f414a2e0
denseformeres <denseformeres@yahoo.com>
2026-04-21 09:31:42 +0100

    1

--- experiments/distributed/ddp.py
+++ experiments/distributed/ddp.py
@@ -42,6 +42,7 @@ class DataParallelDistributedBackend(DistributedBackend):
         args.acc_steps = args.acc_steps // acc_steps_div
         args.batch_size = args.batch_size // (world_size // acc_steps_div)
         args.device = f'cuda:{self.local_rank}'
+        print('./distributed/ddp.py', "args.device = f'cuda:{self.local_rank}'", f'cuda:{self.local_rank}')
         args.seed = args.seed + self.local_rank
         return args
 
