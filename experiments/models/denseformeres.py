df8229b76c918c1514270c280ca0a41eee62d1d4
denseformeres <denseformeres@yahoo.com>
2026-04-25 09:34:37 +0100

    1

--- experiments/models/denseformeres.py
+++ experiments/models/denseformeres.py
@@ -233,7 +233,7 @@ class DenseformerES(nn.Module):
         
         n_layer = config.n_layer
 
-        self.n_cuda0 = max(0, n_layer - 20) 
+        self.n_cuda0 = max(0, n_layer - 19) 
 
         # Now move layers
         for i, block in enumerate(self.transformer["h"]):
